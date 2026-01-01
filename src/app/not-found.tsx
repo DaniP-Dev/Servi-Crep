b6af584561ac3e556763dd9a69eadd79c7e40f22
DaniP-Dev <dperezg23@outlook.com>
2026-01-01 10:48:55 -0500

    error en el deploy

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div>
       <h1>404 - No encontrado</h1>
       <p>La pagina que buscas no existe.</p>
-      <a href="/">volver a home</a>
+      <Link href="/">volver a home</Link>
     </div>
   );
 }
